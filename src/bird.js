4d6af3e9052d8140356d14b423d53d4f16ef97ef
Casper Beyer <casperbeyer@gmail.com>
2014-02-14 23:56:46 +0800

    Refactor code structure.

---
+++ src/bird.js
@@ -0,0 +1,70 @@
+var Bird = (function() {
+   var position;
+   var velocity;
+   var radius;
+   var dead;
+   var mass;
+
+   function Bird(x, y, radius) {
+      this.position = new Vec2(x, y);
+      this.velocity = new Vec2(0, 0);
+      this.radius = radius;
+      this.dead = false;
+      this.mass = 1.3;
+   }
+
+   Object.defineProperty(Bird.prototype, 'x', {
+      get: function () {
+         return this.position.x;
+      },
+      enumerable: true,
+      configurable: true
+   });
+
+   Object.defineProperty(Bird.prototype, 'y', {
+      get: function () {
+         return this.position.y;
+      },
+      enumerable: true,
+      configurable: true
+   });
+
+   Bird.prototype.step = function(dt) {
+      var gravity = -980;
+
+      // apply forces.
+      this.velocity.y += (gravity * this.mass) * dt;
+
+      // terminal velocity:
+      var max = 1500;
+      if (this.velocity.y < -max) {
+         this.velocity.y = -max;
+      }
+
+      this.position.x += this.velocity.x * dt;
+      this.position.y += this.velocity.y * dt;
+   };
+
+   Bird.prototype.flap = function() {
+      // Dead birds don't flap.
+      if (this.dead) {
+         return;
+      }
+
+      this.velocity.y = 460;
+      this.velocity.x = 150;
+   };
+
+   Bird.prototype.die = function() {
+      if (this.dead) {
+         return;
+      }
+
+      this.dead = true;
+      this.velocity.x = 0;
+      this.velocity.y = 250;
+   };
+
+   return Bird;
+})();
+
---
+++ src/terrain.js
@@ -0,0 +1,152 @@
+
+var Terrain = (function() {
+   var rows, columns;
+   var cells;
+   var cellSize;
+
+   function Terrain(columns, rows, cellSize, bleed) {
+      this.rows = rows;
+      this.columns = columns;
+      this.cellSize = cellSize;
+      this.cells = new Array(rows * columns);
+
+      this.fill(0, 0, this.columns, this.rows, -1);
+      this.fill(0, 0, this.columns, 1, 12);
+      this.generate(bleed, this.columns);
+   }
+
+   Terrain.prototype.generate = function(i, length) {
+      while(i < length) {
+         var width = Math.floor(Math.random() * (6 - 2 + 1)) + 2;
+
+         while (i + width > this.columns) {
+            width--;
+         }
+
+         var distance = Math.ceil(128 / this.cellSize);
+         this.generateObstacle(i, i + width, distance);
+
+         var min = Math.floor(128 / this.cellSize);
+         var max = Math.floor(256 / this.cellSize);
+         var seperation = Math.floor(Math.random() * ( max - min + 1)) + min;
+
+         i += width + seperation;
+      }
+   }
+
+   Terrain.prototype.fill = function(x, y, width, height, value) {
+      for (var col = x; col < width; col++) {
+         for (var row = y; row < height; row++) {
+            this.cells[row * this.columns + col] = value;
+         }
+      }
+   };
+
+   Terrain.prototype.generateObstacle = function(start, end, distance) {
+      do {
+         var min = Math.floor(Math.random() * (this.rows - 1));
+         var max = min + (distance + 1);
+      } while((min + max) > this.rows);
+
+      for (var col = start; (col < end) && (col < this.columns); col++) {
+         for (var row = 0; row < this.rows; row++) {
+            var value;
+
+            // if it falls within the gap distance, it is an 'air' cell.
+            if (row > min && row < max) {
+               value = 0;
+            } else {
+               // otherwise, it is some sort of solid tile, determine which.
+               // default to center block.
+               value = 6;
+
+               // check for edges
+               if (col == start) {
+                  // left edge
+                  value = 5;
+
+                  if (row == min) {
+                     // top left
+                     value = 1;
+                  } else if (row == max) {
+                     // bottom left
+                     value = 9;
+                  }
+               } else if(col == end - 1) {
+                  // right edge
+                  value = 7;
+
+                  if (row == min) {
+                     // top right
+                     value = 3;
+                  } else if(row == max) {
+                     // bottom right
+                     value = 11;
+                  }
+               } else {
+                  if (row == min) {
+                     // center top
+                     value = 2;
+                  } else if(row == max) {
+                     // center bottom
+                     value = 10;
+                  }
+               }
+            }
+
+            this.cells[row * this.columns + col] = value;
+         }
+      }
+   };
+
+   // Returns the value at the given world coordiantes.
+   //
+   Terrain.prototype.queryAt = function(x, y) {
+      var col = Math.floor(x / this.cellSize);
+      var row = Math.floor(y / this.cellSize);
+
+      return this.valueAt(col, row);
+   }
+
+   // Returns the value at the given cell coordinates.
+   Terrain.prototype.valueAt = function(x, y) {
+      return this.cells[this.indexAt(x, y)];
+   };
+
+   // Returns the index of the given cell coordinates
+   Terrain.prototype.indexAt = function(col, row) {
+      return row * this.columns + col;
+   };
+
+   Terrain.prototype.intersects = function(obj) {
+      // determine which cell the object is in.
+      var col = Math.floor(obj.x / this.cellSize);
+      var row = Math.floor(obj.y / this.cellSize);
+
+      // number of cells to check.
+      var count = 2; // Math.ceil(obj.radius / this.cellSize);
+
+      for (var i = (col - count); i < (col + count); i++) {
+         for (var j = (row - count); j < (row + count); j++) {
+            if (Math.floor(this.valueAt(i, j)) > 0) {
+
+               // center of the cell in world space.
+               var x = i * this.cellSize;
+               var y = j * this.cellSize;
+
+               var distance = Math.sqrt((x -= obj.x) * x + (y -= obj.y) * y);
+
+               if (distance < (this.cellSize / 2) + obj.radius) {
+
+                  //this.cells[this.indexAt(i, j)] = -1;
+                  return true;
+               }
+            }
+         }
+      }
+
+      return false;
+   };
+
+   return Terrain;
+})();
