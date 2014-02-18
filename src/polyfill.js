fa5cfb83dfc82f71ba827998465c7c4d03cf3ba5
Casper Beyer <casperbeyer@gmail.com>
2014-02-18 18:47:45 +0800

    Add requestAnimationFrame polyfill.

--- src/polyfill.js
+++ src/polyfill.js
@@ -1,4 +1,32 @@
 (function() {
+   var lastTime = 0;
+   var vendors = ['webkit', 'moz'];
+   for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+      window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
+      window.cancelAnimationFrame =
+         window[vendors[x]+'CancelAnimationFrame'] || window[vendors[x]+'CancelRequestAnimationFrame'];
+   }
+
+   if (!window.requestAnimationFrame) {
+      window.requestAnimationFrame = function(callback, element) {
+         var currTime = new Date().getTime();
+         var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+         var id = window.setTimeout(function() { callback(currTime + timeToCall); },
+                                    timeToCall);
+                                    lastTime = currTime + timeToCall;
+                                    return id;
+      };
+   }
+
+   if (!window.cancelAnimationFrame) {
+      window.cancelAnimationFrame = function(id) {
+         clearTimeout(id);
+      };
+   }
+}());
+
+(function() {
+
    // XXX Cocoon does not currently expose CanvasRenderingContext2D
    // Workaround, create a canvas and get its constructor.
    if (!window.CanvasRenderingContext2D) {
