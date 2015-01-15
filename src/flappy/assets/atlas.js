457911e072675bf392fd93e2094314fe9f750a9f
Casper Beyer <caspervonb@qup.io>
2015-01-15 13:21:55 +0800

    Remove waterfall from loadAtlas

--- src/flappy/assets/atlas.js
+++ src/flappy/assets/atlas.js
@@ -1,24 +1,19 @@
-import async from 'async';
 import { loadJson } from './json';
 import { loadImage } from './image';
 
 export function loadAtlas(path, callback) {
-  async.waterfall([
-      // first pass, load the json
-      async.apply(loadJson, path),
+  loadJson(path, function(error, atlas) {
+    if (error) {
+      return callback(error);
+    }
 
-      // second pass, load the image specified in the data
-      function (data, callback) {
-      async.waterfall([
-            // load the image from data
-            async.apply(loadImage, data.image),
-
-            // assign the image, and pass data back to the waterfall
-            function (image, callback) {
-          data.image = image;
-          callback(null, data);
-            },
-         ], callback);
+    loadImage(atlas.image, function(error, image) {
+      if (error) {
+        return callback(error);
       }
-   ], callback);
+
+      atlas.image = image;
+      callback(null, atlas);
+    });
+  });
 }
