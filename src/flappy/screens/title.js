2a261849bb5aef79513a98446a0e8f338f47c2b0
Casper Beyer <caspervonb@qup.io>
2015-01-24 13:30:33 +0800

    Refator title screen

--- src/flappy/screens/title.js
+++ src/flappy/screens/title.js
@@ -5,20 +5,33 @@ import display from 'display';
 export class Title extends Screen {
   constructor(game) {
     super(game);
-    this.playScreen = new Play(game);
+  }
 
-    this.on('keydown', function (key) {
-      this.game.switch(this.playScreen);
-    });
+  keydown(key) {
+    if (key == 32) {
+      this.start();
+    }
+  }
+
+  start() {
+    this.game.switch(new Play(this.game));
   }
 
   draw(time) {
-    // Draw the play state.
-    this.playScreen.draw(time);
+    var background = this.game.assets['image/background'];
+    display.drawImage(
+      background,
+      0, 0, display.target.width, display.target.height,
+      0, 0, background.width, background.height,
+      1, 1, 0, 0
+    );
 
-    // Draw our title overlay.
-    display.reset();
-    display.drawText('100px munro', 'flappy', display.target.width / 2, 100, 'white', 'center');
-    display.drawText('24px munro', 'tap to play', display.target.width / 2, 150, 'white', 'center');
+    var title = this.game.assets['image/title'];
+    display.drawImage(
+      title,
+      display.target.width / 2, display.target.height / 4, title.width, title.height,
+      0, 0, title.width, title.height,
+      1, 1, title.width / 2, title.height / 2
+    );
   }
 }
